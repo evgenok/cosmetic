03b7da5bf028374cda3d23a0367dac7ae3f7c0e2
evgenok <popovevgen18042001@mail.ru>
2023-02-19 18:24:03 +0300

    added slick

--- app/js/main.js
+++ app/js/main.js
@@ -172,3 +172,48 @@ function scrollTopTop() {
       behavior: "smooth"
    })
 }
+
+// ----------------------слайдер slick------------
+
+$('.slick').slick({
+   infinite: true,
+   dots: true,
+   slidesToShow: 7,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   variableWidth: true,
+   responsive: [
+      {
+         breakpoint: 1450,
+         settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+         }
+      },
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+         }
+      },
+      {
+         breakpoint: 600,
+         settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+         }
+      },
+      {
+         breakpoint: 480,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+         }
+      }]
+})
